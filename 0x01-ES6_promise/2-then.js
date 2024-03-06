8f7f7848693d058bf90b4f4444b423a151cb59d7
John Gaitho <johngaitho05@gmail.com>
2024-03-06 15:57:06 +0300

    promise as argument

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,13 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => ({ status: 200, body: 'success' }))
-    .catch((error) => error)
-    .finally(() => {
+    // eslint-disable-next-line no-unused-vars
+    .then((response) => {
       console.log('Got a response from the API');
-    });
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    // eslint-disable-next-line no-unused-vars
+    .catch((error) => new Error());
 }
