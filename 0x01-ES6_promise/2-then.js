5e6b4e462fbfa37faa365aaa030cb5811b587484
John Gaitho <johngaitho05@gmail.com>
2024-03-06 15:59:28 +0300

    test

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    // eslint-disable-next-line no-unused-vars
-    .then((response) => {
+    .then(() => {
       console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success',
       };
     })
-    // eslint-disable-next-line no-unused-vars
-    .catch((error) => new Error());
+    .catch(() => new Error());
 }
