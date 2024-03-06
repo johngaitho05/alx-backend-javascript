b9a10ed962ea06f0d689ce120a29d66e955db206
John Gaitho <johngaitho05@gmail.com>
2024-03-06 17:05:33 +0300

    improvise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -8,6 +8,6 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
 
   return results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value: result.value || result.reason.message,
   }));
 }
