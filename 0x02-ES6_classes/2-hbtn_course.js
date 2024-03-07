181d9e8c6c0e6df74be7a4c83a910fa4f083d7b0
John Gaitho <johngaitho05@gmail.com>
2024-03-07 16:29:13 +0300

    make it ESLINT compliant

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,60 +1,71 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._validateString(name, 'name');
-    this._validateNumber(length, 'length');
-    this._validateArray(students, 'students');
-
+    this.validateString(name, 'name');
+    this.validateNumber(length, 'length');
+    this.validateArray(students, 'students');
+    // eslint-disable-next-line no-underscore-dangle
     this._name = name;
+    // eslint-disable-next-line no-underscore-dangle
     this._length = length;
+    // eslint-disable-next-line no-underscore-dangle
     this._students = students;
   }
 
   // Getter and Setter for 'name'
   get name() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._name;
   }
 
   set name(newName) {
-    this._validateString(newName, 'Name');
+    this.validateString(newName, 'Name');
+    // eslint-disable-next-line no-underscore-dangle
     this._name = newName;
   }
 
   // Getter and Setter for 'length'
   get length() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._length;
   }
 
   set length(newLength) {
-    this._validateNumber(newLength, 'Length');
+    this.validateNumber(newLength, 'Length');
+    // eslint-disable-next-line no-underscore-dangle
     this._length = newLength;
   }
 
   // Getter and Setter for 'students'
   get students() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._students;
   }
 
   set students(newStudents) {
-    this._validateArray(newStudents, 'Students');
+    this.validateArray(newStudents, 'Students');
+    // eslint-disable-next-line no-underscore-dangle
     this._students = newStudents;
   }
 
   // Helper function to validate string
-  _validateString(value, propertyName) {
+  // eslint-disable-next-line class-methods-use-this
+  validateString(value, propertyName) {
     if (typeof value !== 'string') {
       throw new Error(`${propertyName} must be a string`);
     }
   }
 
   // Helper function to validate number
-  _validateNumber(value, propertyName) {
+  // eslint-disable-next-line class-methods-use-this
+  validateNumber(value, propertyName) {
     if (typeof value !== 'number') {
       throw new Error(`${propertyName} must be a number`);
     }
   }
 
   // Helper function to validate array
-  _validateArray(value, propertyName) {
+  // eslint-disable-next-line class-methods-use-this
+  validateArray(value, propertyName) {
     if (!Array.isArray(value)) {
       throw new Error(`${propertyName} must be an array`);
     }
