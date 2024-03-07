87a3d88fbf72d18226dd210a276e712204d33ca8
John Gaitho <johngaitho05@gmail.com>
2024-03-07 19:30:43 +0300

    clone this

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,42 +1,32 @@
-// eslint-disable-next-line no-underscore-dangle
-const _brand = Symbol('brand');
-// eslint-disable-next-line no-underscore-dangle
-const _motor = Symbol('motor');
-// eslint-disable-next-line no-underscore-dangle
-const _color = Symbol('color');
-
-export default class Car {
+class Car {
   constructor(brand, motor, color) {
     // eslint-disable-next-line no-underscore-dangle
-    this[_brand] = brand;
+    this._brand = brand;
     // eslint-disable-next-line no-underscore-dangle
-    this[_motor] = motor;
+    this._motor = motor;
     // eslint-disable-next-line no-underscore-dangle
-    this[_color] = color;
+    this._color = color;
   }
 
-  // Getter for 'brand'
   get brand() {
     // eslint-disable-next-line no-underscore-dangle
-    return this[_brand];
+    return this._brand;
   }
 
-  // Getter for 'motor'
   get motor() {
     // eslint-disable-next-line no-underscore-dangle
-    return this[_motor];
+    return this._motor;
   }
 
-  // Getter for 'color'
   get color() {
     // eslint-disable-next-line no-underscore-dangle
-    return this[_color];
+    return this._color;
   }
 
-  // Method to clone the car
   cloneCar() {
-    // Create a new instance of the class with the same attribute values
-    // eslint-disable-next-line no-underscore-dangle
-    return new Car(this[_brand], this[_motor], this[_color]);
+    // Create a new instance of the TestCar class with the same attribute values
+    return new this.constructor(this.brand, this.motor, this.color);
   }
 }
+
+export default Car;
