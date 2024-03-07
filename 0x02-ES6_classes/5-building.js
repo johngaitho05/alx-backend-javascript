c2d7d64c849080492862556d7674363eacae2b32
John Gaitho <johngaitho05@gmail.com>
2024-03-07 17:23:36 +0300

    eslint

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,10 +3,12 @@ export default class Building {
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
+    // eslint-disable-next-line no-underscore-dangle
     this._sqft = sqft;
   }
 
   get sqft() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._sqft;
   }
 
@@ -14,6 +16,7 @@ export default class Building {
     if (typeof newSqft !== 'number') {
       throw TypeError('Sqft must be a number');
     }
+    // eslint-disable-next-line no-underscore-dangle
     this._sqft = newSqft;
   }
 }
