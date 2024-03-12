2931c04c2ae95d779e1c4497d34c579331eb9c0f
John Gaitho <johngaitho05@gmail.com>
2024-03-12 10:23:56 +0300

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,5 +4,5 @@ export default function cleanSet(set, startString) {
 
   // Join the filtered values into a single string separated by '-'
   const res = filteredValues.map((obj) => obj.slice(startString.length));
-  return startString ? res.join('-') : '';
+  return typeof startString === 'string' && startString.length ? res.join('-') : '';
 }
