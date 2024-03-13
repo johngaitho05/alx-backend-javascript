d8b6fb814c850358790058f064cbd815fec36aaf
John Gaitho <johngaitho05@gmail.com>
2024-03-13 15:52:38 +0300

    print teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,12 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    const firstInitial = firstName.charAt(0).toUpperCase();
+    return `${firstInitial}. ${lastName}`;
+};
