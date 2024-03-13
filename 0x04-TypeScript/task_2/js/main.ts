4ac888720afe51d553fce6de07ef07764876a606
John Gaitho <johngaitho05@gmail.com>
2024-03-13 16:10:10 +0300

    subjects

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -57,3 +57,15 @@ function executeWork(employee: Director | Teacher): string {
         return (employee as Teacher).workTeacherTasks();
     }
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else if (todayClass === 'History') {
+        return 'Teaching History';
+    } else {
+        return 'Invalid subject';
+    }
+}
