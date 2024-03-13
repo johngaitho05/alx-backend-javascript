de8c7ef53e9360e9e5f066d34fdfd55d49420223
John Gaitho <johngaitho05@gmail.com>
2024-03-13 15:58:31 +0300

    student class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,3 +19,24 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
     const firstInitial = firstName.charAt(0).toUpperCase();
     return `${firstInitial}. ${lastName}`;
 };
+
+interface StudentClassConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    constructor(public firstName: string, public lastName: string) {}
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
