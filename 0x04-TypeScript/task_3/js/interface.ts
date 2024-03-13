5906f4c9d3d6acde118837952efc88a553087fe1
John Gaitho <johngaitho05@gmail.com>
2024-03-13 19:53:40 +0300

    ambient namespaces

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,12 @@
+
+// Define the RowID type
+type RowID = number;
+
+// Define the RowElement interface
+interface RowElement {
+    firstName: string;
+    lastName: string;
+    age?: number;
+}
+
+export { RowID, RowElement };
